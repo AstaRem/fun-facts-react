c830e0332f382f6099801d999b56cae28867aaf3
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-07-09 16:19:04 +0100

    made and styled MainContent

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="page-wrapper">
       <Navbar />
       <MainContent />
 
-    </>
+    </div>
   )
 }
 
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,6 +1,17 @@
 
 function MainContent(){
-    return (<h1>Main content here</h1>)
+    return (
+        <main className="main-container">
+            <h1 className="main-title">Fun facts about React</h1>
+            <ul className="main-facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+        )
 }
 
 export default MainContent;
