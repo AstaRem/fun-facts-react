38aecb32adfda82744d302758c6450ead743cc0a
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-07-09 13:00:15 +0100

    made and styled navigation component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,18 @@
+import reactPNG from '../assets/react-img.png'
 
 function Navbar(){
-    return (<h1>navbar will be here</h1>)
+    return (
+        <nav>
+            <div id="brand">
+                <img src={reactPNG} alt="react logo" id="react-logo"></img>
+                <h3>React Facts</h3>
+            </div>
+            <div>
+                <h4 className="subtitle">React Course - Project 1</h4>
+            </div>
+        </nav>
+    )
+    
 }
 
 export default Navbar;
